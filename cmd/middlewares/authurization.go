dee4f1411c42693eb30b40567d762266013efd94
Eftakhairul Islam <eftakhairul@gmail.com>
2019-11-24 16:10:09 -0500

    authorization middeware is done

--- cmd/middlewares/authurization.go
+++ cmd/middlewares/authurization.go
@@ -1,8 +1,23 @@
 package middlewares
 
+import "github.com/gin-gonic/gin"
+
 //AuthorizationMiddleware authorizes request
 func AuthorizationMiddleware() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		c.Next()
+		context.Next()
+
+		/*
+			authHeader := context.GetHeader("Authorization")
+			if len(authHeader) == 0 {
+				libs.NewError(c, http.StatusUnauthorized, errors.New("Authorization is required Header"))
+				context.Abort()
+			}
+			if authHeader != config.Config.ApiKey {
+				libs.NewError(context, http.StatusUnauthorized, fmt.Errorf("this user isn't authorized to this operation: api_key=%s", authHeader))
+				context.Abort()
+			}
+			context.Next()
+		*/
 	}
 }
